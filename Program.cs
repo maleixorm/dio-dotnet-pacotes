014544dabdffaf186a3544daf5e125b445b14294
Marcos Aleixo <maknytz@gmail.com>
2025-10-01 22:38:48 -0300

    Criando um arquivo JSON

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using Newtonsoft.Json;
 
 Venda v1 = new Venda(1, "Material de escritório", 25.00M);
 
-string serializado = JsonConvert.SerializeObject(v1);
+string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+
+File.WriteAllText("Arquivos/vendas.json", serializado);
 
 Console.WriteLine(serializado);
