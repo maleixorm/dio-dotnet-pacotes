60d22758e5ab706fcf01768284d5ad29ab4de1a0
Marcos Aleixo <maknytz@gmail.com>
2025-10-01 22:34:17 -0300

    Criando e serializando um objeto do tipo Venda

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dio_dotnet_pacotes.Models;
+using System.Globalization;
+using Newtonsoft.Json;
+
+Venda v1 = new Venda(1, "Material de escritório", 25.00M);
+
+string serializado = JsonConvert.SerializeObject(v1);
+
+Console.WriteLine(serializado);
