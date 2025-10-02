8de96da47d7e4bd7dd7da7695499720205a963a8
Marcos Aleixo <maknytz@gmail.com>
2025-10-01 22:31:59 -0300

    Criando a classe Venda

---
+++ Models/Venda.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dio_dotnet_pacotes.Models
+{
+    public class Venda
+    {
+        public Venda(int id, string produto, decimal preco)
+        {
+            Id = id;
+            Produto = produto;
+            Preco = preco;
+        }
+        public int Id { get; set; }
+        public string Produto { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
